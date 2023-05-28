e3024e6cc381f7b42cbe851affa839b8438efc25
帅 李 <2363502880@qq.com>
2023-05-28 22:51:57 +0800

    init

---
+++ testImage/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for (int i = 0; i < 1000; i++)
+{
+    await Task.Delay(1000);
+    Console.WriteLine("current "+i+" -> 1000");
+}
+Console.WriteLine("end");
